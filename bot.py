972b0d345f2ecbb44b96a8473a5e82b5d2aa2ed1
Lazizbek Quvondiqov <lazizbekquvondiqov2003@gmail.com>
2026-01-31 17:12:46 +0500

    new admin

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ logger = logging.getLogger(__name__)
 
 # Environment variables
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = int(os.getenv('ADMIN_ID'))
+ADMIN_IDS = [int(id_str.strip()) for id_str in os.getenv('ADMIN_ID').split(',')]
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 BOT_USERNAME = "@kino_zakaz2026_bot"  # O'zingizni bot usernameni shu yerga yozing
 
